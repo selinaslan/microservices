f2311328cb73b372e330ca7f4dd144988063a8b7
selinaslan <selin.aslan7@gmail.com>
2019-10-09 14:49:03 +0300

    Zuul added

--- price-calculator-service/src/main/java/com/slnaslan/microservces/pricecalculatorservice/ItemPriceServiceProxy.java
+++ price-calculator-service/src/main/java/com/slnaslan/microservces/pricecalculatorservice/ItemPriceServiceProxy.java
@@ -9,12 +9,12 @@ import com.slnaslan.microservces.pricecalculatorservice.bean.PriceCalculatorBean
 
 //@FeignClient(name="item-price-service", url="localhost:8000")
 //@FeignClient(name="item-price-service")
-@FeignClient(name="item-price-service")
-@RibbonClient(name="item-price-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
+@RibbonClient(name = "item-price-service")
 public interface ItemPriceServiceProxy {
 
-	@GetMapping("item-price/item/{item}")
-	public PriceCalculatorBean retrieveItemPrice(@PathVariable String item) ;
+//	@GetMapping("item-price/item/{item}")
+	@GetMapping("item-price-service/item-price/item/{item}")
+	public PriceCalculatorBean retrieveItemPrice(@PathVariable String item);
 
-	
 }
--- price-calculator-service/src/main/java/com/slnaslan/microservces/pricecalculatorservice/controller/PriceCalculatorController.java
+++ price-calculator-service/src/main/java/com/slnaslan/microservces/pricecalculatorservice/controller/PriceCalculatorController.java
@@ -20,18 +20,18 @@ public class PriceCalculatorController {
 	@Autowired
 	private ItemPriceServiceProxy proxy;
 	
-	@GetMapping("price-calculator/item/{item}/quantity/{quantity}")
-	public PriceCalculatorBean calculateTotalAmount(@PathVariable String item,@PathVariable BigDecimal quantity) {
-		
-		Map<String,String> uriVariables= new HashMap<String,String>();
-		uriVariables.put("item",item);
-		
-		ResponseEntity<PriceCalculatorBean> forEntity = new RestTemplate().getForEntity("http://localhost:8000/item-price/item/{item}", PriceCalculatorBean.class,uriVariables );
-		
-		PriceCalculatorBean response = forEntity.getBody();
-		
-		return new PriceCalculatorBean(response.getId(),item,response.getPrice(),quantity,quantity.multiply(response.getPrice()),0);
-	}
+//	@GetMapping("price-calculator/item/{item}/quantity/{quantity}")
+//	public PriceCalculatorBean calculateTotalAmount(@PathVariable String item,@PathVariable BigDecimal quantity) {
+//		
+//		Map<String,String> uriVariables= new HashMap<String,String>();
+//		uriVariables.put("item",item);
+//		
+//		ResponseEntity<PriceCalculatorBean> forEntity = new RestTemplate().getForEntity("http://localhost:8000/item-price/item/{item}", PriceCalculatorBean.class,uriVariables );
+//		
+//		PriceCalculatorBean response = forEntity.getBody();
+//		
+//		return new PriceCalculatorBean(response.getId(),item,response.getPrice(),quantity,quantity.multiply(response.getPrice()),0);
+//	}
 
 	@GetMapping("price-calculator-feign/item/{item}/quantity/{quantity}")
 	public PriceCalculatorBean calculateTotalAmountFeign (@PathVariable String item,@PathVariable BigDecimal quantity) {
